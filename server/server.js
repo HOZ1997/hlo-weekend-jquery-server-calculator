f9ff112c45646c2a46d4cf371e65c53073e0c804
Heather Osborne <theoz308@comcast.net>
2022-04-30 13:34:36 -0500

    server working

---
+++ server/server.js
@@ -0,0 +1,15 @@
+// requires
+const express = require('express');
+const app = express ();
+
+// app uses
+
+
+//globals
+const port = 5001;
+
+
+//spin up server
+app.listen(port, ()=>{
+console.log ('server up on', port);     
+} )
